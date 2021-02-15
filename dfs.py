455514da51d6a3f2e67b9021bb423af05668e900
s <myae3080@naver.com>
2021-02-15 20:07:13 +0900

    dfs study

--- dfs.py
+++ dfs.py
@@ -7,17 +7,35 @@ graph = {
     'F': []
 }
 
-def dfs(graph, start, visited):
-    visited.append(start)
+# def dfs(graph, start, visited):
+#     visited.append(start)
 
-    print(visited)
+#     print(visited)
     
-    for v in graph[start]:
-        if v not in visited:
-            dfs(graph, v, visited)
+#     for v in graph[start]:
+#         if v not in visited:
+#             dfs(graph, v, visited)
 
-dfs(graph, 'A', [])
+# dfs(graph, 'C', [])
 
-# stack으로 구현
-# def dfs_list(graph, start):
+# visited(list)를 parameter로 안 받는 방식
+def dfs_list(graph, start):
+    visited, nodeList = [], []
+
+    nodeList.append(start)
     
+    while nodeList:
+        node = nodeList.pop(0)
+
+        if node in visited:
+            # 확인용
+            print(node)
+        else:
+            visited.append(node)
+            # start에 근접한 node 추가
+            nodeList.extend(graph[node])
+    
+    return visited
+
+#  정상적인 탐색 X, 수정 필요.
+print(dfs_list(graph, 'C'))
