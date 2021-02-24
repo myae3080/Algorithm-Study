677a87920797ff25c998db6b7f8ca7c1af5a086c
Test <myae3080@naver.com>
2021-02-25 08:14:52 +0900

    collections

--- python/datastructuretest.py
+++ python/datastructuretest.py
@@ -1,6 +1,9 @@
 import collections
 
+# deque의 경우, FIFO, LILO 둘 다 구현할 수 있는 자료구조.
+# 
 test_deque = collections.deque(['A', 'B', 'C'])
+test_deque2 = collections.deque(['A', 'B', 'C'])
 test_deque.append('D')
 
 print(test_deque)
@@ -8,3 +11,9 @@ print(test_deque)
 test_deque.appendleft('AA')
 
 print(test_deque)
+
+test_deque.append('ef')
+test_deque2.extend('ef')
+
+print('test_deque : ', test_deque)
+print('test_deque2 : ', test_deque2)
