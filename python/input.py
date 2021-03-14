12bd3b5d65b99621bd5928dbd9500d794d04f885
Test <myae3080@naver.com>
2021-03-15 07:32:02 +0900

    input study

---
+++ python/input.py
@@ -0,0 +1,12 @@
+# 
+data = input()
+
+print('data : ', data)
+
+data1, data2 = map(int, input().split())
+
+print('data1 : ', data1, 'data2 : ', data2)
+
+data_list = list(map(int, input().split()))
+
+print('data_list : ', data_list)
