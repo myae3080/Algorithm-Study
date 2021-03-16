d3f26debe7ddcc23dd4a916ec0672ced317dc50d
s <myae3080@naver.com>
2021-03-16 19:08:33 +0900

    input

--- python/input.py
+++ python/input.py
@@ -1,12 +1,14 @@
-# 
+# 입력 받은 한 줄을 string으로 할당.
 data = input()
 
 print('data : ', data)
 
+# space가 포함된 입력을 분리해서 int형으로 할당.
 data1, data2 = map(int, input().split())
 
 print('data1 : ', data1, 'data2 : ', data2)
 
+# space가 포함됨 입력을 분리해서 int형의 list로 할당.
 data_list = list(map(int, input().split()))
 
 print('data_list : ', data_list)
