71be0fe480406d968ee5fcb1da078fc948c2ba18
Test <myae3080@naver.com>
2021-04-03 12:50:20 +0900

    testing

--- python/input.py
+++ python/input.py
@@ -9,6 +9,7 @@ data1, data2 = map(int, input().split())
 print('data1 : ', data1, 'data2 : ', data2)
 
 # space가 포함됨 입력을 분리해서 int형의 list로 할당.
+# 입력 받은 값에 따른 type 지정?
 data_list = list(map(int, input().split()))
 
 print('data_list : ', data_list)
