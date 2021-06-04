eca3ee516df5764434d13f7a93f23e0384857139
s <myae3080@naver.com>
2021-06-04 12:23:03 +0900

    3rd commit 15552 cpp

--- Baekjoon/cpp/15552.cpp
+++ Baekjoon/cpp/15552.cpp
@@ -1,20 +1,20 @@
 // fast io
 
 #include <iostream>
+using namespace std;
 
 int main(void) {
     int count, num1, num2 = 0;
 
     // deactivate sync between iostream and stdio
-	
-	std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
-	std::cin >> count;
+	cin >> count;
 	
 	for(int i = 0; i < count; i++) {
-		std::cin >> num1 >> num2;
-		std::cout << num1 + num2 << '\n';
+		cin >> num1 >> num2;
+		cout << num1 + num2 << '\n';
 	}    
     
     return 0;
