12c5bef31472e80321f6578fbcf7f638de0728de
myae3080 <37862243+myae3080@users.noreply.github.com>
2021-06-04 12:47:18 +0900

    Update 15552.cpp

--- Baekjoon/cpp/15552.cpp
+++ Baekjoon/cpp/15552.cpp
@@ -7,15 +7,15 @@ int main(void) {
     int count, num1, num2 = 0;
 
     // deactivate sync between iostream and stdio
-	ios::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
     
-	cin >> count;
+    cin >> count;
 	
-	for(int i = 0; i < count; i++) {
-		cin >> num1 >> num2;
-		cout << num1 + num2 << '\n';
-	}    
+    for(int i = 0; i < count; i++) {
+	cin >> num1 >> num2;
+	cout << num1 + num2 << '\n';
+    }    
     
     return 0;
 }
