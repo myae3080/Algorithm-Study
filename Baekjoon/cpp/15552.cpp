796efc3ca28075176b637a9e49e3552d90a9c9e3
s <myae3080@naver.com>
2021-06-04 12:51:53 +0900

    2nd commit 15552 cpp

--- Baekjoon/cpp/15552.cpp
+++ Baekjoon/cpp/15552.cpp
@@ -11,11 +11,11 @@ int main(void) {
     cin.tie(NULL);
     
     cin >> count;
-	
-	for(int i = 0; i < count; i++) {
-		cin >> num1 >> num2;
-		cout << num1 + num2 << '\n';
-	}    
+
+    for (int i = 0; i < count; i++) {
+        cin >> num1 >> num2;
+        cout << num1 + num2 << '\n';
+    }
     
     return 0;
 }
