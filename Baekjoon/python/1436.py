854d9de0569a50884a4cdeda62dcafa14c9a08d2
s <myae3080@naver.com>
2021-06-06 20:13:40 +0900

    1st commit 1436 python

---
+++ Baekjoon/python/1436.py
@@ -0,0 +1,14 @@
+# brute force
+
+# input
+count = int(input())
+
+start_num = 666
+
+while count != 0:
+    if "666" in str(start_num):
+        count -= 1
+    
+    start_num += 1
+
+print(start_num - 1)
