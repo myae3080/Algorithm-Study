bebfec298073a82952df190080db8b1a4d95d1da
s <myae3080@naver.com>
2021-06-07 00:08:41 +0900

    1st commit 1934 python

---
+++ Baekjoon/python/1934.py
@@ -0,0 +1,21 @@
+# math, least common multiple
+
+# input
+count = int(input())
+
+# Euclidean algorithm
+def gcd(large_num, small_num):
+    if small_num == 0:
+        return large_num
+
+    if large_num % small_num == 0:
+        return small_num
+    else:
+        return gcd(small_num, large_num % small_num)
+
+for i in range(count):
+    num1, num2 = map(int, input().split())
+
+    gcd_num = gcd(num2, num1)
+
+    print(gcd_num * (num1 // gcd_num) * (num2 // gcd_num))
