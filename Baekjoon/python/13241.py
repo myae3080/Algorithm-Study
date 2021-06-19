b89be41b2a96655344e2d45e91e4fc951bc9bb4e
s <myae3080@naver.com>
2021-06-20 08:23:33 +0900

    2nd commit 13241 python

--- Baekjoon/python/13241.py
+++ Baekjoon/python/13241.py
@@ -1,6 +1,10 @@
 # lcm
 
 def gcd(large_num, small_num):
+    # 함수를 사용 하는 사용자가 실수를 했을 때를 대비한 코딩
+    if large_num < small_num:
+        large_num, small_num = small_num, large_num
+
     if small_num == 0:
         return large_num
     
@@ -11,8 +15,5 @@ def gcd(large_num, small_num):
 
 # input
 a, b = map(int, input().split())
-
-if b > a:
-    a, b = b, a
 
 print(int(a * b / gcd(a, b)))
