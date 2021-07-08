023473afb2ca0dc8961c1179f22da9db39e675c3
s <myae3080@naver.com>
2021-07-08 14:35:04 +0900

    1st commit 1158 python

---
+++ Baekjoon/python/1158.py
@@ -0,0 +1,20 @@
+# data structure, queue
+
+# input
+n, k = map(int, input().split())
+
+josephus = [i for i in range(1, n + 1)]
+result = []
+idx = k - 1
+
+# TODO indexError
+while josephus:
+    if idx >= len(josephus):
+        idx %= len(josephus)
+
+    result.append(josephus.pop(idx))
+
+    # index 조정
+    idx = idx - 1 + k
+
+print(str(result).replace('[', '<').replace(']', '>'))
