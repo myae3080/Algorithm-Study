c1eaf3a64d49d6b55fc5d9b5effa9c65eb5d7941
s <myae3080@naver.com>
2021-07-19 17:21:12 +0900

    1st commit 9372 python

---
+++ Baekjoon/python/9372.py
@@ -0,0 +1,16 @@
+# graph theory, tree
+
+import sys
+
+# input
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    # input
+    country, plain = map(int, input().split())
+
+    for r in range(plain):
+        # input
+        a, b = map(int, input().split())
+
+    print(country - 1)
