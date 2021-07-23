24bd5220aee9aca2b58b5b86e8308f47bb0db221
s <myae3080@naver.com>
2021-07-24 08:43:26 +0900

    2nd commit 15649 python

--- Baekjoon/python/15649.py
+++ Baekjoon/python/15649.py
@@ -3,6 +3,16 @@
 # input
 n, m = map(int, input().split())
 
+'''
+    두 번째 풀이
+'''
+import itertools
+
+[print(e) for e in list(map(' '.join, itertools.permutations([str(i) for i in range(1, n + 1)], m)))]
+
+'''
+    첫 번째 풀이
+'''
 st = []
 
 # 재귀 실행 순서를 이해하는게 중요.
