c101ac52d3e40772ece4ca139be34fa10ad137dd
s <myae3080@naver.com>
2021-08-01 17:40:31 +0900

    1st commit 5338 python

---
+++ Baekjoon/python/5338.py
@@ -0,0 +1,7 @@
+# source : https://www.acmicpc.net/problem/5338
+
+print("       _.-;;-._")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
