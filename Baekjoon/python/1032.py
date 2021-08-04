9c179b88b40466113d8b67a9517ddddc4029a9b3
s <myae3080@naver.com>
2021-08-04 13:56:28 +0900

    1st commit 1032 python

---
+++ Baekjoon/python/1032.py
@@ -0,0 +1,15 @@
+input_list = []
+
+[input_list.append(input()) for i in range(int(input()))]
+
+pattern_list = list(input_list[0])
+length = len(pattern_list)
+
+for i in range(1, len(input_list)):
+    temp = input_list[i]
+
+    for j in range(length):
+        if pattern_list[j] != temp[j]:
+            pattern_list[j] = '?'
+
+print(''.join(pattern_list))
