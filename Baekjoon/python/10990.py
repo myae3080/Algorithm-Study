7bd458ff661b828428d041a3062eddfee82dda25
s <myae3080@naver.com>
2021-08-31 15:08:03 +0900

    1st commit 10990 python

---
+++ Baekjoon/python/10990.py
@@ -0,0 +1,11 @@
+# source : https://www.acmicpc.net/problem/10990
+# star
+
+# input
+n = int(input())
+
+for i in range(n):
+    if i == 0:
+        print(' ' * (n - 1) + '*')
+    else:
+        print(' ' * (n - i - 1) + '*' + ' ' * (2 * i - 1) + '*')
