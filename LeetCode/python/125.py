3dbef6aec68cd68678113dd2d33d1560504d48fc
s <myae3080@naver.com>
2021-11-28 19:22:31 +0900

    2nd commit 125 python

--- LeetCode/python/125.py
+++ LeetCode/python/125.py
@@ -1,6 +1,7 @@
 # source : https://leetcode.com/problems/valid-palindrome/
 
 class Solution:
+    # 1 / 52ms
     def isPalindrome(self, s: str) -> bool:
         str_list = []
         
@@ -14,4 +15,16 @@ class Solution:
             if str_list[i] != str_list[len(str_list) - i - 1]:
                 return False
 
-        return True            
+        return True
+    
+    # 2 / 52ms
+    def isPalindrome2(self, s: str) -> bool:
+        str_list = []
+        
+        for c in s:
+            if c.isalnum():
+                str_list.append(c.lower())
+        
+        new_str = ''.join(str_list)
+        
+        return new_str == new_str[::-1]
