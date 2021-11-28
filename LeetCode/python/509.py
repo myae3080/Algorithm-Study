126c4c182e3954dd7e126a09a8237986410a893b
s <myae3080@naver.com>
2021-11-29 07:10:55 +0900

    1st commit 509 python

---
+++ LeetCode/python/509.py
@@ -0,0 +1,12 @@
+# source : https://leetcode.com/problems/fibonacci-number/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        fibo = [0] * 31
+        fibo[1], fibo[2] = 1, 1
+        
+        if n > 2:
+            for i in range(2, n + 1):
+                fibo[i] = fibo[i - 1] + fibo[i - 2]
+                
+        return fibo[n]
