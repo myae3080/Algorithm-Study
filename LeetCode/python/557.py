d2981dc6cae590bd24113c63149aed35c9915c45
s <myae3080@naver.com>
2021-12-16 11:02:25 +0900

    1st commit 557 python

---
+++ LeetCode/python/557.py
@@ -0,0 +1,5 @@
+# source : https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join([word[::-1] for word in list(s.split())])
