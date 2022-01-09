38c20565796b3890287ace1f50fb4f97583e95cd
s <myae3080@naver.com>
2022-01-09 17:07:47 +0900

    2nd commit 53 python

--- LeetCode/python/53.py
+++ LeetCode/python/53.py
@@ -9,3 +9,12 @@ class Solution:
                 nums[i] += temp_num
         
         return max(nums)
+
+    def maxSubArray2(self, nums: list[int]) -> int:
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1] + nums[i], nums[i])
+            
+        return max(dp)
