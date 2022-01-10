4276dd65bd65fc058d5d99557e339c2f39d925b8
s <myae3080@naver.com>
2022-01-10 16:49:49 +0900

    1st commit 349 python

---
+++ LeetCode/python/349.py
@@ -0,0 +1,5 @@
+# source : https://leetcode.com/problems/intersection-of-two-arrays/
+
+class Solution:
+    def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        return set(nums1).intersection(nums2)
