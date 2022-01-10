1537e825f3480772976458b11e0adc0df467e85a
s <myae3080@naver.com>
2022-01-10 10:41:54 +0900

    1st commit 88 python

---
+++ LeetCode/python/88.py
@@ -0,0 +1,13 @@
+# source : https://leetcode.com/problems/merge-sorted-array/
+
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        del nums1[m:]
+        
+        nums1.extend(nums2[:n])
+        
+        nums1.sort()
