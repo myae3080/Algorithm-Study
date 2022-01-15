09ccf40d727d701410698902326fb28cb065e9ea
s <myae3080@naver.com>
2022-01-15 15:56:49 +0900

    1st commit 1654 python

---
+++ Baekjoon/python/1654.py
@@ -0,0 +1,22 @@
+# source : https://www.acmicpc.net/problem/1654
+# binary search
+
+import sys
+
+# input
+k, n = map(int, input().split())
+lans = [int(input()) for _ in range(k)]
+
+start, end = 1, max(lans)
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    
+    if sum([lan // mid for lan in lans]) < n:
+        end = mid - 1
+    else:
+        result = mid
+        start = mid + 1
+    
+print(result)
