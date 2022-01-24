31d6c22941639b1931450e98f33e072f673a6a01
s <myae3080@naver.com>
2022-01-24 11:38:40 +0900

    2nd commit 70 python

--- LeetCode/python/70.py
+++ LeetCode/python/70.py
@@ -1,6 +1,7 @@
 # source : https://leetcode.com/problems/climbing-stairs/
 
 class Solution:
+    # 1st
     def climbStairs(self, n: int) -> int:
         dp = [0] * (n + 1)
         dp[1] = 1
@@ -10,5 +11,14 @@ class Solution:
             
             for i in range(3, n + 1):
                 dp[i] = dp[i - 2] + dp[i - 1]
+        
+        return dp[n]
+    
+    # 2nd
+    def climbStairs(self, n: int) -> int:
+        dp = [1] * (n + 1)
+            
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 2] + dp[i - 1]
         
         return dp[n]
