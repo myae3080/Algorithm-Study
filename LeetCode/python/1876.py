35368e3dc3cfa61980d0dd782a124dd2351b90c3
s <myae3080@naver.com>
2022-04-02 19:42:13 +0900

    2nd commit 1876 python

--- LeetCode/python/1876.py
+++ LeetCode/python/1876.py
@@ -11,3 +11,14 @@ class Solution:
                 result += 1
                 
         return result
+    
+    def countGoodSubstrings2(self, s: str) -> int:
+        result = 0
+        
+        for i in range(len(s) - 2):
+            first, second, third = s[i], s[i + 1], s[i + 2]
+            
+            if first != second and first != third and second != third:
+                result += 1
+                
+        return result
