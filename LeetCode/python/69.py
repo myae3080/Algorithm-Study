56724e066e1bee742d832d3a15eaf4917c219810
s <myae3080@naver.com>
2022-04-10 21:32:31 +0900

    2nd commit 69 python

--- LeetCode/python/69.py
+++ LeetCode/python/69.py
@@ -3,5 +3,27 @@
 import math
 
 class Solution:
+    # 24ms
     def mySqrt(self, x: int) -> int:
         return math.trunc(x ** 0.5)
+    
+    # 40ms
+    def mySqrt2(self, x: int) -> int:
+        start, end = 0, math.ceil(x / 2)
+        result = 0
+        
+        while start <= end:
+            mid = (start + end) // 2
+            sqr = mid * mid
+
+            if sqr == x:
+                result = mid
+                break
+            elif sqr < x:
+                start = mid + 1
+                
+                result = mid
+            else:
+                end = mid - 1
+                
+        return result
