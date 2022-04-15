28cf537d43f7c66733ee0cbe0eb75d208871a7df
s <myae3080@naver.com>
2022-04-15 20:23:02 +0900

    2nd commit 350 python

--- LeetCode/python/350.py
+++ LeetCode/python/350.py
@@ -1,6 +1,7 @@
 # source : https://leetcode.com/problems/intersection-of-two-arrays-ii/
 
 class Solution:
+    # 68ms
     def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
         result = []
         
@@ -8,5 +9,23 @@ class Solution:
             if nums2.count(num) > 0:
                 result.append(num)
                 del nums2[nums2.index(num)]
+                
+        return result
+    
+    # 48ms
+    def intersect2(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        result = []
+        dic = {}
+        
+        for n in nums1:
+            if n not in dic:
+                dic[n] = 1
+            else:
+                dic[n] += 1
+                
+        for n in nums2:
+            if n in dic and dic[n] > 0:
+                result.append(n)
+                dic[n] -= 1
                 
         return result
