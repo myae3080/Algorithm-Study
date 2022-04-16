7c21350d1ebf165fbe678932e752085a3fbd7668
s <myae3080@naver.com>
2022-04-16 17:56:35 +0900

    2nd commit 278 python

--- LeetCode/python/278.py
+++ LeetCode/python/278.py
@@ -1,5 +1,8 @@
 # source : https://leetcode.com/problems/first-bad-version/
 
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
 class Solution:
     def firstBadVersion(self, n):
         start, end = 0, n
