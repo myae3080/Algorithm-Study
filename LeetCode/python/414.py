50549448c2534f2336cd7755ac9c86b9454e1326
s <myae3080@naver.com>
2022-05-09 05:33:54 +0900

    1st commit 414 python

---
+++ LeetCode/python/414.py
@@ -0,0 +1,10 @@
+# source : https://leetcode.com/problems/third-maximum-number/
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums_set = sorted(set(nums), reverse = True)
+        
+        if len(nums_set) >= 3:
+            return nums_set[2]
+        else:
+            return nums_set[0]
