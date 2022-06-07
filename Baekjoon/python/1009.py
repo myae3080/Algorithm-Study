2d53e1a5d7d409be8635d710ecabb8449be94cb2
s <myae3080@naver.com>
2022-06-08 07:30:24 +0900

    1st commit 1009 python

---
+++ Baekjoon/python/1009.py
@@ -0,0 +1,25 @@
+# source : https://www.acmicpc.net/problem/1009
+# math
+# TODO : 시간초과
+import sys
+
+# input
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    # input
+    a, b = map(int, input().split())
+    a %= 10
+    
+    if a == 0:
+        print(10)  
+    elif a == 1 or a == 5 or a == 6:
+        print(a)
+    elif a == 4 or a == 9:
+        b %= 2
+        
+        print((a ** 2) % 10) if b == 0 else print((a ** b) % 10)
+    else:
+        b %= 4
+        
+        print((a ** 4) % 10) if b == 0 else print((a ** b) % 10)
