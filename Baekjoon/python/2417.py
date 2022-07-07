3f7d8f24c7e896810c0ff1f68ccaee42daed89ca
s <myae3080@naver.com>
2022-07-08 05:20:40 +0900

    1st commit 2417 python

--- Baekjoon/python/2417.py
+++ Baekjoon/python/2417.py
@@ -1,3 +1,28 @@
-import math
+# source : https://www.acmicpc.net/problem/2417
+# math, binary search
 
-print(math.ceil(math.sqrt(int(input()))))
+# input
+n = int(input())
+
+start, end = 0, int((2 ** 63) ** 0.5)
+result = 0
+
+while start < end:
+    mid = (start + end) // 2
+    sq = mid ** 2
+    
+    if sq > n:
+        end = mid - 1
+    elif sq < n:
+        start = mid + 1
+    else:
+        result = mid
+        break
+
+if result == 0:
+    if end ** 2 < n:
+        result = end + 1     
+    else: 
+        result = end
+
+print(result)
