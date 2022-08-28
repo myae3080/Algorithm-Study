9fcfc05ea260da73649a7c149929dc839b49721c
s <myae3080@naver.com>
2022-08-29 05:20:59 +0900

    1st commit 14914 python

---
+++ Baekjoon/python/14914.py
@@ -0,0 +1,8 @@
+# source : https://www.acmicpc.net/problem/14914
+
+# input
+a, b = map(int, input().split())
+
+for i in range(1, a + 1):
+    if a % i == 0 and b % i == 0:
+        print(i, a // i, b // i)
