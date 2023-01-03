955b2762494af2e677aad69e61916d12e811b9d7
s <myae3080@naver.com>
2023-01-03 19:21:41 +0900

    1st commit 23810 python

---
+++ Baekjoon/python/23810.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/23810
+
+# input
+n = int(input())
+
+[print('@' * 5 * n) for i in range(n)]
+[print('@' * n) for i in range(n)]
+[print('@' * 5 * n) for i in range(n)]
+[print('@' * n) for i in range(n)]
+[print('@' * n) for i in range(n)]
