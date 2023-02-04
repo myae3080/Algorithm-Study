2a970a2b558a8376bca6ca4c69006dc3b954f481
myae3080 <myae3080@naver.com>
2023-02-04 16:52:24 +0900

    2nd commit 17863 go

--- Baekjoon/go/17863.go
+++ Baekjoon/go/17863.go
@@ -10,9 +10,7 @@ func main() {
 	// input
 	fmt.Scanf("%s", &input)
 
-	var strFive = "5"
-
-	if string(input[0]) == strFive && string(input[1]) == strFive && string(input[2]) == strFive {
+	if input[:3] == "555" {
 		fmt.Printf("YES")
 	} else {
 		fmt.Printf("NO")
