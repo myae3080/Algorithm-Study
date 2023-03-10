b889aab199a923603f8ff85b90a721bea2a7c48b
myae3080 <myae3080@naver.com>
2023-03-10 20:14:55 +0900

    1st commit 22193 python

---
+++ Baekjoon/python/22193.py
@@ -0,0 +1,9 @@
+# source : https://www.acmicpc.net/problem/22193
+
+def main():
+    # input
+    n, m = map(int, input().split())
+    print(int(input()) * int(input()))
+
+if __name__ == '__main__':
+    main()
