e81e8c27bc9ddc9c453f0cfa7e78ecc54f84c419
s <myae3080@naver.com>
2023-03-29 16:25:34 +0900

    1st commit 24264 python

---
+++ Baekjoon/python/24264.py
@@ -0,0 +1,11 @@
+# source : https://www.acmicpc.net/problem/24264
+
+def main():
+    # input
+    n = int(input())
+    
+    print(n * n)
+    print(2)
+    
+if __name__ == '__main__':
+    main()
