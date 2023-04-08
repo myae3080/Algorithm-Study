4b6a8b0f0a0615505ce231647d35c9db48b847c1
myae3080@naver.com <myae3080@naver.com>
2023-04-08 19:11:02 +0900

    1st commit 27323 go

---
+++ Baekjoon/go/27323.go
@@ -0,0 +1,15 @@
+// source : https://www.acmicpc.net/problem/27323
+
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int
+
+	// input
+	fmt.Scanln(&x)
+	fmt.Scanln(&y)
+
+	fmt.Println(x * y)
+}
