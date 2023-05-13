9a3dab5230672188e15ca9a73c29757fcff1c386
s <myae3080@naver.com>
2023-05-13 16:05:39 +0900

    1st commit 24265 python

---
+++ Baekjoon/python/24265.py
@@ -0,0 +1,11 @@
+# source : https://www.acmicpc.net/problem/24265
+
+def main():
+    # input
+    n = int(input())
+    
+    print(sum([i for i in range(1, n)]))
+    print(2)
+    
+if __name__ == '__main__':
+    main()
