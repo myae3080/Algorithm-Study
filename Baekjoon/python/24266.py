cb68b774acc18f82ee67e67832cf8f49da3c1fef
s <myae3080@naver.com>
2023-05-16 19:51:44 +0900

    1st commit 24266 python

---
+++ Baekjoon/python/24266.py
@@ -0,0 +1,11 @@
+# source : https://www.acmicpc.net/problem/24266
+
+def main():
+    # input
+    n = int(input())
+    
+    print(n ** 3)
+    print(3)
+    
+if __name__ == '__main__':
+    main()
