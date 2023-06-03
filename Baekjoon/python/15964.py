1e69fd607bf8a9f93bd80aa9b2d8fb1b08520df3
s <myae3080@naver.com>
2023-06-03 21:07:42 +0900

    1st commit 15964 python

---
+++ Baekjoon/python/15964.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/15964
+
+def main():
+    # input
+    a, b = map(int, input().split())
+    
+    print(a ** 2 - b ** 2)
+    
+if __name__ == '__main__':
+    main()
