ef591c6f55bdee1e345366775849a9fbcb0414bc
s <myae3080@naver.com>
2023-06-18 15:10:39 +0900

    1st commit 12780 python

---
+++ Baekjoon/python/12780.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/12780
+
+def main():
+    # input
+    string, target = input(), input()
+    
+    print(string.count(target))
+    
+if __name__ == '__main__':
+    main()
