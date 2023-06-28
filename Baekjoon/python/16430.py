45d5a29ae35f0e3a37292f15d8a24236b45e7ef2
s <myae3080@naver.com>
2023-06-28 20:58:25 +0900

    1st commit 16430 python

---
+++ Baekjoon/python/16430.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/16430
+
+def main():
+    # input
+    a, b = map(int, input().split())
+    
+    print(b - a, b)
+    
+if __name__ == '__main__':
+    main()
