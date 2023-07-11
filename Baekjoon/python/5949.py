830fe4ae9f314c30ea5b9c1343d38ed317d59a2a
s <myae3080@naver.com>
2023-07-11 21:59:58 +0900

    1st commit 5949 python

---
+++ Baekjoon/python/5949.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/5949
+
+def main():
+    # input
+    number = int(input())
+    
+    print(format(number, ','))
+    
+if __name__ == '__main__':
+    main()
