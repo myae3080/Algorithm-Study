b0cf32b17016641348976336c29580501f366136
s <myae3080@naver.com>
2023-07-24 20:03:59 +0900

    1st commit 15372 python

---
+++ Baekjoon/python/15372.py
@@ -0,0 +1,13 @@
+# source : https://www.acmicpc.net/problem/15372
+
+import sys
+input = sys.stdin.readline
+
+def main():
+    # input
+    for _ in range(int(input())):
+        # input
+        print(int(input()) ** 2)
+        
+if __name__ == '__main__':
+    main()
