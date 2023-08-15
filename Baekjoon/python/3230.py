aa863f93ba0d84362e9b5711a3f5159d379db352
s <myae3080@naver.com>
2023-08-15 22:19:21 +0900

    2nd commit 3230 python

--- Baekjoon/python/3230.py
+++ Baekjoon/python/3230.py
@@ -6,15 +6,18 @@ def main():
     
     # 첫 번째 경주
     first_ranks = [0]
-    # input
-    [first_ranks.insert(int(input()), i) for i in range(1, n + 1)]
+    for i in range(1, n + 1):
+        # input
+        first_ranks.insert(int(input()), i)
         
     # 두 번째 경주
     second_ranks = [0]
-    # input
-    [second_ranks.insert(int(input()), first_ranks[i]) for i in range(m, 0, -1)]
+    for i in range(m, 0, -1):
+        # input
+        second_ranks.insert(int(input()), first_ranks[i])
     
-    [print(player) for player in second_ranks[1:4]]
+    for player in second_ranks[1:4]:
+        print(player)
 
 if __name__ == '__main__':
     main()
