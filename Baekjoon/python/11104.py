e24ab080431cfe8bbc056fa75607b085d88425c9
s <myae3080@naver.com>
2023-11-20 21:02:08 +0900

    1st commit 11104 python

---
+++ Baekjoon/python/11104.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/11104
+
+def main():
+    # input
+    for _ in range(int(input())):
+        # input
+        print(int(input(), 2))
+
+if __name__ == '__main__':
+    main()
