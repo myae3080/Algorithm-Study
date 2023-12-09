b26198705df8e455491cc904dbe7785d8ee2d63f
s <myae3080@naver.com>
2023-12-09 19:16:45 +0900

    1st commit 6887 python

---
+++ Baekjoon/python/6887.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/6887
+
+def main():
+    # input
+    n = int(input())
+    
+    print('The largest square has side length %d.' % int(n ** 0.5))
+
+if __name__ == '__main__':
+    main()
