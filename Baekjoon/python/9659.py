d8ca2301765c7b4166cb8c67436c9198eb99ff7d
s <myae3080@naver.com>
2024-02-04 16:53:57 +0900

    1st commit 9659 python

---
+++ Baekjoon/python/9659.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/9659
+
+def main():
+    # input
+    n = int(input())
+    
+    print('CY' if n % 2 == 0 else 'SK')
+
+if __name__ == '__main__':
+    main()
