4dfd7e6f03c41ce12757d5e164adc50262fe8beb
s <myae3080@naver.com>
2024-03-11 07:26:44 +0900

    1st commit 31472 python

---
+++ Baekjoon/python/31472.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/31472
+
+def main():
+    # input
+    w = int(input())
+    
+    print(int((w * 2) ** 0.5) * 4)
+
+if __name__ == '__main__':
+    main()
