7116e114bd7d16db6fe2d3123989d5046afb20d9
s <myae3080@naver.com>
2024-03-30 20:40:31 +0900

    1st commit 2606 python

--- Baekjoon/python/2606.py
+++ Baekjoon/python/2606.py
@@ -1,33 +1,39 @@
-# graph theory, graph search, dfs, bfs
+# source : https://www.acmicpc.net/problem/2606
 
 import sys
-
-'''
-    컴퓨터를 오름차순으로 정렬을 해서 재방문에 대한 고려를 해결할 수 있을 줄 알았으나, 빠지는 부분이 생겨 정렬없이 세팅 후,
-    1번 부터 다 뒤지는 방식으로 해결
-'''
-# input
 input = sys.stdin.readline
-com_count = int(input())
 
-virus_dict = {}
-visited_list = []
-
-def check_com(start, visited):
-    for j in virus_dict[start]:
-        if j not in visited_list:
-            visited.append(j)
-            check_com(j, visited)
-
-for i in range(1, 101):
-    virus_dict[i] = set()
-
-for i in range(int(input())):
+def main():
     # input
-    com1, com2 = map(int, input().rstrip().split())
-    virus_dict[com1].add(com2)
-    virus_dict[com2].add(com1)
+    coms = int(input())
+    pairs = int(input())
+    
+    network = {}
+    for _ in range(pairs):
+        # input
+        a, b = map(int, input().split())
+        
+        if a in network:
+            network[a].add(b)
+        else:
+            network[a] = {b}
+        
+        if b in network:
+            network[b].add(a)
+        else:
+            network[b] = {a}
 
-check_com(1, visited_list)
-
-print(len(visited_list) - 1)
+    visited = [0] * (coms + 1)
+    def checkVirus(start):
+        visited[start] = 1
+        if start in network:
+            for c in network[start]:
+                if visited[c] == 0:
+                    checkVirus(c)
+    
+    checkVirus(1)
+    
+    print(visited.count(1) - 1)
+    
+if __name__ == '__main__':
+    main()
