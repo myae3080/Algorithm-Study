ab8424d7c717e83cf288dc180a104f16a560e081
s <myae3080@naver.com>
2024-04-28 17:54:11 +0900

    2nd commit 30684 typescript

--- Baekjoon/typescript/30684.ts
+++ Baekjoon/typescript/30684.ts
@@ -3,6 +3,6 @@
 // input
 const input: Array<string> = require('fs').readFileSync('example.txt').toString().replace(/\r/g, "").split('\n');
 
-const names = input.slice(1);
+const names: Array<string> = input.slice(1);
 
-console.log(names.filter((name) => name.length === 3).sort()[0]);
+console.log(names.filter((name: string) => name.length === 3).sort()[0]);
