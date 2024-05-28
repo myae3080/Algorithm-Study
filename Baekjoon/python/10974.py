ca68ca9b38d8d6d4719f7eb304708942ab918838
s <myae3080@naver.com>
2024-05-28 21:59:24 +0900

    2nd commit 10974 python

--- Baekjoon/python/10974.py
+++ Baekjoon/python/10974.py
@@ -1,21 +1,13 @@
 # source : https://www.acmicpc.net/problem/10974
 
+from itertools import permutations
+
 def main():
     # input
     n = int(input())
     
-    backTracking([], n)
-
-def backTracking(arr: list, n: int):
-    if len(arr) == n:
-        print(" ".join(map(str, arr)))
-        return
-
-    for i in range(1, n + 1):
-        if i not in arr:
-            arr.append(i)
-            backTracking(arr, n)
-            arr.pop()
+    for tu in list(permutations([i for i in range(1, n + 1)], n)):
+        print(" ".join(map(str, tu)))
 
 if __name__ == '__main__':
     main()
