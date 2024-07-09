8079f7677197c416e3fd259e70ffe002aeab92a1
s <myae3080@naver.com>
2024-07-09 22:12:36 +0900

    1st commit 29751 python

---
+++ Baekjoon/python/29751.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/29751
+
+def main():
+    # input
+    W, H = map(int, input().split())
+    
+    print(W * H * 0.5)
+
+if __name__ == '__main__':
+    main()
