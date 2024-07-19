6c6cd3397239643eb591c3105da07717af694535
s <myae3080@naver.com>
2024-07-19 18:39:15 +0900

    1st commit 6438 python

---
+++ Baekjoon/python/6438.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/6438
+
+def main():
+    # input
+    for _ in range(int(input())):
+        # input
+        print(input()[::-1])
+
+if __name__ == '__main__':
+    main()
