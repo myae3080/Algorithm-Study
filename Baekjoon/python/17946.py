377e8a201291422c1cfcd01ca19907dce7c3c498
s <myae3080@naver.com>
2024-07-21 12:56:25 +0900

    1st commit 17946 python

---
+++ Baekjoon/python/17946.py
@@ -0,0 +1,17 @@
+# source : https://www.acmicpc.net/problem/17946
+
+import sys
+input = sys.stdin.readline
+
+def main():
+    # input
+    N = int(input())
+    
+    for _ in range(N):
+        # input
+        cutting = int(input())
+        
+        print(1)
+
+if __name__ == '__main__':
+    main()
