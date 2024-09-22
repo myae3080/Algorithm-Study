6ef6797e2f756cfb121df620e1583535c1801f11
myae3080 <myae3080@naver.com>
2024-09-22 20:08:55 +0900

    1st commit 15272 python

---
+++ Baekjoon/python/15272.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/15272
+
+def main():
+    # input
+    word = input()
+    
+    print('hiss' if 'ss' in word else 'no hiss')
+
+if __name__ == '__main__':
+    main()
