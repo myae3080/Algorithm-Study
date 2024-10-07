6d989596fa99ed21206cf992a1d4fd83d329eaaf
myae3080 <myae3080@naver.com>
2024-10-07 20:36:18 +0900

    1st commit 32297 python

---
+++ Baekjoon/python/32297.py
@@ -0,0 +1,11 @@
+# source : https://www.acmicpc.net/problem/32297
+
+def main():
+    # input
+    N = int(input())
+    S = input()
+    
+    print('YES' if 'gori' in S else 'NO')
+
+if __name__ == '__main__':
+    main()
