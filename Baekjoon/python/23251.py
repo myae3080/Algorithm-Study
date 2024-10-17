8dba9d1cb6de77dd06317d8420fbc0409a8c36c0
myae3080 <myae3080@naver.com>
2024-10-17 18:10:24 +0900

    1st commit 23251 python

---
+++ Baekjoon/python/23251.py
@@ -0,0 +1,15 @@
+# source : https://www.acmicpc.net/problem/23251
+
+import sys
+input = sys.stdin.readline
+
+def main():
+    # input
+    for _ in range(int(input())):
+        # input
+        k = int(input())
+        
+        print(23 * k)
+
+if __name__ == '__main__':
+    main()
