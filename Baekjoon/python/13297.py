5e22828410b75e3402ecaea9f015a21e1eb6919e
myae3080 <myae3080@naver.com>
2024-12-23 17:24:23 +0900

    1st commit 13297 python

---
+++ Baekjoon/python/13297.py
@@ -0,0 +1,13 @@
+# source : https://www.acmicpc.net/problem/13297
+
+import sys
+input = sys.stdin.readline
+
+def main():
+    # input
+    for _ in range(int(input())):
+        # input
+        print(len(input().rstrip()))
+
+if __name__ == '__main__':
+    main()
