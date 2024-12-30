ae5fd1263ce209fe05597182859a4afc877f13d0
myae3080 <37862243+myae3080@users.noreply.github.com>
2024-12-30 09:08:40 +0900

    Create 6840.py
    first commit python 6840

---
+++ Baekjoon/python/6840.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/6840
+
+def main():
+    #input
+	nums = [int(input()) for _ in range(3)]
+
+    print(sorted(nums)[1])
+
+if __name__ == '__main__':
+    main()
