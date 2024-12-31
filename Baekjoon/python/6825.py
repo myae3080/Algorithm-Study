759b8d7c1c2af8cb82d5eef6b9baf3b281b551d2
myae3080 <37862243+myae3080@users.noreply.github.com>
2024-12-31 10:01:41 +0900

    Create 6825.py
    1st commit 6825 python

---
+++ Baekjoon/python/6825.py
@@ -0,0 +1,17 @@
+# source : https://www.acmicpc.net/problem/6825
+
+def main():
+	# input
+	weight, height = float(input()), float(input())
+
+    bmi = round(weight / (height * height), 1)
+
+    if bmi > 25:
+		print('Overweight')
+    elif bmi > 18.5:
+		print('Normal weight')
+    else:
+        print('Underweight')
+
+if __name__ == '__main__':
+	main()
