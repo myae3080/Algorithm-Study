3a55fef3138b31365697ac332dc3bb184605b177
myae3080 <myae3080@naver.com>
2025-02-25 18:15:13 +0900

    1st commit 13597 python

---
+++ Baekjoon/python/13597.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/13597
+
+def main():
+    # input
+    A, B = map(int, input().split())
+    
+    print(max(A, B))
+
+if __name__ == '__main__':
+    main()
