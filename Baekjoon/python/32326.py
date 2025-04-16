98ea7166852bb6633984ebc089b67636d3fe9f72
myae3080 <myae3080@naver.com>
2025-04-16 21:30:56 +0900

    1st commit 32326 python

---
+++ Baekjoon/python/32326.py
@@ -0,0 +1,8 @@
+# source : https://www.acmicpc.net/problem/32326
+
+def main():
+    # input
+    print(3 * int(input()) + 4 * int(input()) + 5 * int(input()))
+
+if __name__ == '__main__':
+    main()
