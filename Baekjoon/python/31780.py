3fbb56f677042a15a302d09051011c43040e9595
myae3080 <myae3080@naver.com>
2025-05-17 09:45:09 +0900

    1st commit 31780 python

---
+++ Baekjoon/python/31780.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/31780
+
+def main():
+    # input
+    X, M = map(int, input().split())
+    
+    print(X * (M + 1))
+
+if __name__ == '__main__':
+    main()
