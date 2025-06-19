5ef0519e3566b9526ab59f3ba930452118b116f7
myae3080 <myae3080@naver.com>
2025-06-19 18:52:12 +0900

    1st commit 31610 python

---
+++ Baekjoon/python/31610.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/31610
+
+def main():
+    # input
+    A, B, C = int(input()), int(input()), int(input())
+    
+    print(A * B + C)
+
+if __name__ == '__main__':
+    main()
