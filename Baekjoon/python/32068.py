6aa963f0aa80a6934411ae9de347aba928e9eb3e
my <myae3080@naver.com>
2025-08-25 05:46:33 +0900

    1st commit 32068 python

---
+++ Baekjoon/python/32068.py
@@ -0,0 +1,20 @@
+# sourc : https://www.acmicpc.net/problem/32068
+
+import sys
+
+input = sys.stdin.readline
+
+def main():
+    # input
+    for _ in range(int(input())):
+        # input
+        L, R, S = map(int, input().split())
+
+        L_distance, R_distance = S - L, R - S
+        if R_distance > L_distance:
+            print(L_distance * 2 + 1)
+        else:
+            print(R_distance * 2)
+
+if __name__ == '__main__':
+    main()
