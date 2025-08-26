2592ca24a0508e731705f8bd999906cf20a95672
my <myae3080@naver.com>
2025-08-26 19:01:00 +0900

    1st commit 3765 python

---
+++ Baekjoon/python/3765.py
@@ -0,0 +1,12 @@
+# source : https://www.acmicpc.net/problem/3765
+
+def main():
+    try:
+        while 1:
+            # input
+            print(input())
+    except:
+        return
+
+if __name__ == '__main__':
+    main()
