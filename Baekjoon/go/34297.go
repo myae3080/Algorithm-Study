82c502efe318437c215cdcd22e6172d420a2cd41
my <myae3080@naver.com>
2025-10-15 13:00:15 +0900

    1st commit 34297 go

---
+++ Baekjoon/go/34297.go
@@ -0,0 +1,18 @@
+// source : https://www.acmicpc.net/problem/34297
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var m, k, c int
+
+	// input
+	fmt.Scan(&m)
+	fmt.Scan(&k)
+	fmt.Scan(&c)
+
+	fmt.Println(m * c)
+}
