38559ce2feb908ec28dc8e146c833b1585044708
my <myae3080@naver.com>
2025-11-09 08:32:16 +0900

    1st commit 33702 python

---
+++ Baekjoon/python/33702.py
@@ -0,0 +1,13 @@
+# source : https://www.acmicpc.net/problem/33702
+
+def main():
+    # input
+    K = int(input())
+
+    if K % 2 == 0:
+        print(0)
+    else:
+        print(8)
+
+if __name__ == '__main__':
+    main()
