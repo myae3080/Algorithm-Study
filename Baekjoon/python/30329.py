21aeb4d98f427230e0d514564a45ac9d3ad1b9bb
my <myae3080@naver.com>
2025-11-25 21:29:46 +0900

    1st commit 30329 python

---
+++ Baekjoon/python/30329.py
@@ -0,0 +1,17 @@
+# source : https://www.acmicpc.net/problem/30329
+
+def main():
+    # input
+    s = input()
+
+    result = 0
+    for i in range(len(s)):
+        if s[i:i + 4] == 'kick':
+            result += 1
+        else:
+            continue
+    
+    print(result)
+
+if __name__ == '__main__':
+    main()
