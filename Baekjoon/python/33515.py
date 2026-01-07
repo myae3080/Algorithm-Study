e5de2353621445165927b2d693081af6946fec13
my <myae3080@naver.com>
2026-01-07 22:05:40 +0900

    1st commit 33515 python

---
+++ Baekjoon/python/33515.py
@@ -0,0 +1,10 @@
+# source : https://www.acmicpc.net/problem/33515
+
+def main():
+    # input
+    t1, t2 = map(int, input().split())
+
+    print(min(t1, t2))
+
+if __name__ == '__main__':
+    main()
