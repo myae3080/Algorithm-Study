9cd64dc96e177eccea5f1899f91cee177a22fa72
my <myae3080@naver.com>
2026-01-11 15:52:04 +0900

    1st commit 31636 python

---
+++ Baekjoon/python/31636.py
@@ -0,0 +1,11 @@
+# source : https://www.acmicpc.net/problem/31636
+
+def main():
+    # input
+    N = int(input())
+    S = input()
+
+    print('Yes' if 'ooo' in S else 'No')
+
+if __name__ == '__main__':
+    main()
